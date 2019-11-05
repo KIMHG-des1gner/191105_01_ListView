946aaf11ec42e881d55febeeaba69ae6b373214a
KIMHG-des1gner <des1gner__@naver.com>
2019-11-05 20:50:14 +0900

    방 데이터 클래스 추가  - 메인 액티비티 : 데이터 추가를 별개 메쏘드로 작성  - sum메쏘드로 코틀린만의 특이한 문법 확인

--- app/src/main/java/com/tjoeun/a191105_01_listview/datas/RoomData.kt
+++ app/src/main/java/com/tjoeun/a191105_01_listview/datas/RoomData.kt
@@ -1,10 +1,29 @@
 package com.tjoeun.a191105_01_listview.datas
 
-class RoomData {
+import android.util.Log
+
+class RoomData(price:Int, adress:String, floor:Int) {
+
 
     var price:Int = 0
     var adress:String = ""
     var floor:Int = 1 // 음수 : 지하. -층 : 지하1층 0층 : 반지하
 
+    init {
+        this.price = price
+        this.adress = adress
+        this.floor = floor
+
+    }
+
+    constructor() : this(0, "미입력" 0) {
+        Log.d("룸데이터", "보조생성자(빈칸) 호출")
+
+    }
+
+    constructor(adress: String) : this (-1, address, -1) {
+        Log.d("룸데이터", "보조생성자(빈칸) 호출")
+
+    }
 
 }
